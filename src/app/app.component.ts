bad697ad612cc1388a7315edeb2c01483965b130
Danielle Magrini <danielle.magrini@hotmail.com>
2024-02-26 22:05:53 -0300

    home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./Componentes/login/login.component";
+import { NavbarComponent } from './Componentes/navbar/navbar.component';
+import { HomeComponent } from './Componentes/home/home.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, LoginComponent, NavbarComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'sicoob';
